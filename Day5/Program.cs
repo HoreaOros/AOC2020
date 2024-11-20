6a6925c4112218b5e9ce08141e95c3739aa8e291
Horea Oros <horea.oros@gmail.com>
2024-11-20 11:54:01 +0200

    add Day 5, Day6 solutions

---
+++ Day5/Program.cs
@@ -0,0 +1,47 @@
+﻿#region Input processing
+string lines = File.ReadAllText("input.txt");
+string[] seats = lines.Split(Environment.NewLine);
+#endregion
+
+
+#region Part1
+int maxim = 0;
+// BBFBFBFLRL
+// 01100
+List<int> lst = new();
+foreach (string seat in seats)
+{
+    int row = 0, column = 0, bit;
+    for (int i = 0; i < 7; i++)
+    {
+        if (seat[i] == 'F')
+            bit = 0;
+        else
+            bit = 1;
+        row = row * 2 + bit;
+    }
+    for(int i = 7; i < 10; i++)
+    {
+        if (seat[i] == 'L')
+            bit = 0;
+        else
+            bit = 1;
+        column = column * 2 + bit;
+    }
+    int seatID = row * 8 + column;
+    lst.Add(seatID);
+    maxim = Math.Max(maxim, seatID);
+         
+}
+Console.WriteLine(maxim);
+#endregion
+
+
+#region Part2
+lst.Sort();
+for(int i = 0; i < lst.Count - 1; i++)
+    if (lst[i + 1] - lst[i] == 2)
+        Console.WriteLine(lst[i] + 1);
+#endregion
+
+
---
+++ Day6/Program.cs
@@ -0,0 +1,19 @@
+﻿#region Input processing
+string text = File.ReadAllText("input.txt");
+string[] groups = text.Split("\r\n\r\n");
+Console.WriteLine();
+#endregion
+
+#region Part1
+int sum = 0;
+foreach (string group in groups)
+{
+    HashSet<char> q = new HashSet<char>();
+    foreach (char c in group)
+        if(char.IsLetter(c))
+            q.Add(c);
+ 
+    sum += q.Count;
+}
+Console.WriteLine(sum);
+#endregion
