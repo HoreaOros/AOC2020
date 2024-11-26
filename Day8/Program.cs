aceac73ea126b5fcd4720c1debd490618d1e7d03
Horea Oros <horea.oros@gmail.com>
2024-11-26 12:28:37 +0200

    add Day9 solution

--- Day8/Program.cs
+++ Day8/Program.cs
@@ -13,30 +13,62 @@ foreach (string line in lines)
 #endregion
 
 #region Part1
-int PC = 0; // program counter
-int accumulator = 0;
-HashSet<int> SEEN = new();
-do
-{
-    SEEN.Add(PC);
-    var currentInstr = program[PC];
-    switch (currentInstr.instr)
-    {
-        case "nop":
-            PC++;
-            break;
-        case "acc":
-            accumulator += currentInstr.arg;
-            PC++;
-            break;
-        case "jmp":
-            PC += currentInstr.arg;
-            break;
-    }
-} while (!SEEN.Contains(PC));
+(int accumulator, bool result) = Part1(program);
 Console.WriteLine(accumulator);
+
+static (int, bool) Part1(List<(string instr, int arg)> program)
+{
+    int PC = 0; // program counter
+    int accumulator = 0;
+    HashSet<int> SEEN = new();
+    do
+    {
+        SEEN.Add(PC);
+        var currentInstr = program[PC];
+        switch (currentInstr.instr)
+        {
+            case "nop":
+                PC++;
+                break;
+            case "acc":
+                accumulator += currentInstr.arg;
+                PC++;
+                break;
+            case "jmp":
+                PC += currentInstr.arg;
+                break;
+        }
+        if (PC == program.Count)
+            return (accumulator, true);
+    } while (!SEEN.Contains(PC));
+    return (accumulator, false);
+}
 #endregion
 
 
 #region Part2
+for(int i = 0; i < program.Count; i++)
+{
+    if (program[i].instr == "acc")
+        continue;
+
+    string bck = program[i].instr;
+    if (program[i].instr == "nop")
+        program[i] = ("jmp", program[i].arg);
+    else
+        program[i] = ("nop", program[i].arg);
+
+
+    (int acc, bool res) = Part1(program);
+
+    if(res == true)
+    {
+        Console.WriteLine(acc);
+        break;
+    }
+
+
+    program[i] = (bck, program[i].arg);
+
+}
 #endregion
---
+++ Day9/Program.cs
@@ -0,0 +1,60 @@
+﻿#region Input parsing
+string text = File.ReadAllText("input.txt");
+int range = 25;
+string[] lines = text.Split(Environment.NewLine);
+ulong[] nums = lines.Select(x => ulong.Parse(x)).ToArray();
+#endregion
+
+
+
+#region Part1
+List<ulong> lst = new List<ulong>();
+for (int i = 0; i < range; i++)
+    lst.Add(nums[i]);
+ulong p1Result = 0;
+for(int i = range; i < nums.Length; i++)
+{
+    if (TwoSum(lst, nums[i]))
+    {
+        lst.RemoveAt(0);
+        lst.Add(nums[i]);
+        continue;
+    }
+    else
+    {
+        p1Result = nums[i];
+        Console.WriteLine(nums[i]);
+        break;
+    }
+}
+
+bool TwoSum(List<ulong> lst, ulong v)
+{
+    for(int i = 0; i < lst.Count; i++)
+        for(int j = i + 1; j < lst.Count; j++)
+            if (lst[i] + lst[j] == v)
+                return true;
+    return false;
+}
+#endregion
+
+#region Part2
+ulong[] sums = new ulong[nums.Length + 1];
+sums[0] = 0;
+ulong p2Result = 0;
+for (int i = 1; i < sums.Length; i++)
+{
+    sums[i] = sums[i - 1] + nums[i - 1];
+}
+bool found = false;
+for(int i = 1; !found && i < sums.Length; i++)
+    for(int j = i + 1; !found && j < sums.Length; j++)
+    {
+        if (j > i && sums[j] - sums[i - 1] == p1Result)
+        {
+            p2Result = nums[(i-1)..(j-1)].Min() + nums[(i - 1)..(j-1)].Max();
+            found = true;
+        }
+    }
+Console.WriteLine(p2Result);
+#endregion
